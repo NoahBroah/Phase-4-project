693af53b09edc420e26298afa5c3832a52790a21
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-12-27 15:42:37 -0800

    Update Custom Controller Action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ include ActionController::Cookies
   end
 
   def current_user
-    @user = User.find_by_id(session[:user_id])
+    User.find_by_id(session[:user_id])
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
     #Allow a user to singup and login
     def create
-        user = User.create(user_params)
-        if user.valid?
-            session[:user_id] = user.id #This Line logs in the user
-            render json: user, status: :created
+        @user = User.create(user_params)
+        if @user.valid?
+            login_user #This Line logs in the user
+            render json: @user, status: :created
         else
-            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
