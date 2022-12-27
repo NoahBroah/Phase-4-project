c4c46966cac9370aca8148bf759f45a5998e9920
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-12-27 15:41:16 -0800

    Custom Actions created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::API
 include ActionController::Cookies
 
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count] }
+  def login_user
+    session[:user_id] = @user.id
   end
+
+  def current_user
+    @user = User.find_by_id(session[:user_id])
+  end
+  
 end
