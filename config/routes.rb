62f312cc43bc788e4a1d29c3f883ceb55d844e5b
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-12-27 14:51:32 -0800

    Only Create Route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 post '/login', to: 'sessions#create'
 delete '/logout', to: 'sessions#destroy'
