861fc8b85c34a8c6f74febd59eedac9bf7bca9f6
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-12-27 14:45:10 -0800

    Session Controller actions Create and Destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < ApplicationController
-    
+    def create
+        user = User.find_by(username: params[:username])
+        if user.&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { errors: ["Invalid username or password"]}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete
+        head :no_content
+    end
 end
