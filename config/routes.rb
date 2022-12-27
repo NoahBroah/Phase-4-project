8e557e9a90c2c20aa1d2de8c6d21e65818e82383
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-12-27 15:31:58 -0800

    Namespace Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  #Sessions Routes
 post '/login', to: 'sessions#create'
 delete '/logout', to: 'sessions#destroy'
+
+# User Routes
+post '/signup', to: 'users#create'
 
 
 
