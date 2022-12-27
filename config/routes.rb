a482fb223a3f8c4dc3419fc4900e7628ae2ad2a2
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-12-27 14:48:37 -0800

    Create POST and DELETE routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
+
+
+
 
   # Defines the root path route ("/")
   # root "articles#index"
