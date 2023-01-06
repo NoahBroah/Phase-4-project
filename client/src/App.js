0c7b32d07ef5103bb498e878b25487a2e12538ac
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-01-06 11:26:06 -0800

    Added Faker, Reset data base, testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-// import "bootstrap/dist/css/bootstrap.min.css"
+
+import "./bootstrap.css"
 import Navbar from './Navbar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from './Signup';
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# u1 = User.create({ username:'Noah', password:'mypassword'})
-# u2 = User.create({ username:'Jerm', password:'mypassword'})
-# p1 = Project.create({title:"Art Project", description:"I would like to start a collaborative art project done by multiple people in different art forms", number_of_people:10, creator:"Noah", user_id:3})
-# p2 = Project.create({title:"Fairytale Book", description:"I would like to start a collaborative to create a fairytale book", number_of_people:3, creator:"Sheda", user_id:7})
-# n1 = Note.create({comment:"I'd love to work on this", user_id:3, project_id: 43})
+require 'faker'
+
+u1 = User.create!({ username:'Noah', password:'mypassword'})
+u2 = User.create!({ username:'Jerm', password:'mypassword'})
+users = [u1,u2]
+
+for a in 1..5 do
+    for user in users do
+        # create project
+        project = user.projects.create!({title:Faker::Movie.title, description:Faker::Quote.famous_last_words, number_of_people:Faker::Number.number(digits: 2)})
+        for b in 1..5 do
+            # create notes for the project
+            user.notes.create!({comment:Faker::Quote.famous_last_words, project_id: project.id})
+        end
+    end
+end   
