a3f5e2ddf153dd43267289c076095aa4e37f6194
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-01-19 13:09:05 -0800

    Custom Methods for getting projects by a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,31 @@ class User < ApplicationRecord
     has_many :projects
     has_many :notes, dependent: :destroy
     has_many :projects, through: :notes
+
+#     1. my_created_projects method which will return all projects that the user is the owner of
+#     2. my_noted_projects method which will return all projects that the user left a comment on
+
+    # def my_created_projects
+    #     #Grab all the projects
+    #     projects = Project.all
+    #     #Find the current user
+    #     user = self
+    #     #Iterate through all of the projects
+    #     projects.each do |project|
+    #         #Find only the projects who belong to that user
+    #         if project.user_id == user.id
+    #             #Return just those projects
+    #             return project
+    #         end
+    #     end
+    # end
+
+    # def my_noted_projects
+    #     #Grab all the projects
+    #     projects = Project.all
+
+    # end
+
+
+
 end
