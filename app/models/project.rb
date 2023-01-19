aab118bc72c6ef14300c85a42e896a36d516e61c
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-01-19 13:08:23 -0800

    Validate projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,7 @@ class Project < ApplicationRecord
     belongs_to :user
     has_many :notes, dependent: :destroy
     has_many :users, through: :notes
-
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :number_of_people, presence: true
 end
